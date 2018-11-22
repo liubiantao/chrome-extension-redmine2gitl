3ad2138c0e63c5ac1adc262d251480f9fe1e52d6
haitao.yin <haitao.yin@alo7.com>
2018-11-22 16:29:38 +0800

    feat: add copy button

--- js/content-script.js
+++ js/content-script.js
@@ -1,8 +1,33 @@
+const addCopyButton = (node, title) => {
+  const button = document.createElement('button')
+  button.innerText = 'Copy'
+  button.setAttribute('data-clipboard-text', title)
+  node.appendChild(button)
+
+  const clipboard = new ClipboardJS(button)
+
+  clipboard.on('success', e => {
+    e.clearSelection()
+
+    button.innerText = 'Copy success'
+    setTimeout(() => {
+      button.innerText = 'Copy'
+    }, 1000)
+  })
+
+  clipboard.on('error', e => {
+    console.error('Action:', e.action)
+    console.error('Trigger:', e.trigger)
+  })
+}
+
 const addRedmineIdToIssuesTitle = () => {
   const id = document.location.pathname.replace('/issues/', '')
   const titleNode = document.querySelector('.subject h3')
   if (id && titleNode) {
     titleNode.innerText += ` (REDMINE-${id})`
+    const title = titleNode.innerHTML
+    addCopyButton(titleNode, title)
   }
 }
 
